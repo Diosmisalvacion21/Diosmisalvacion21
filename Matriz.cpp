8582114a4bf4f50a4f90f775a13942d233dc43ca
Jordy Angulo <85962253+JordyAngulo21@users.noreply.github.com>
2021-06-22 09:50:01 -0500

    Add files via upload

---
+++ Matriz.cpp
@@ -0,0 +1,99 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int c;
+float matriz1[10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
+float matriz2[10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
+float suma[10],resta[10],producto[10],division[10];
+
+
+cout<<"OPERACIONES BASICAS CON MATRICES:\n\n";
+cout<<"¿QUE OPERACION DESEAS REALIZAR? \n";
+cout<<"1. Suma \n";
+cout<<"2. Resta \n";
+cout<<"3. Multiplicacion \n";
+cout<<"4. Division \n";
+cout<<"\n";
+
+//PRESENTACIÓN DE LAS MATRICES
+cout<<"LAS MATRICES SON:\n";
+
+for (int i=0;i<10;i++)
+{
+	cout<<matriz1[i]<<" - ";
+}
+cout<<"\n";
+
+for (int i=0;i<10;i++)
+{
+	cout<<matriz2[i]<<" - ";
+}
+
+cout<<"\n\n";
+cout<<"******************\n\n";
+
+cout<<"ELIJA LA OPERACION QUE DESEA REALIZAR:\n\n";
+cin>>c;
+cout<<"\n";
+
+
+//SUMA DE LAS MATRICES
+if(c==1)
+
+{
+	cout<<"USTED ESCOJIO LA OPCION DE SUMA\n\n";
+	cout<<"EL RESULTADO ES:\n\n";
+	for (int i=0;i<10;i++)
+{
+	suma[i]=matriz1[i]+matriz2[i];
+	cout<<suma[i]<<"\n";
+}
+}
+
+cout<<"******************\n\n";
+
+//RESTA DE LAS MATRICES
+if(c==2)
+
+{
+	cout<<"USTED ESCOJIO LA OPCION DE RESTA\n\n";
+	cout<<"EL RESULTADO ES:\n\n";
+	for (int i=0;i<10;i++)
+{
+	resta[i]=matriz1[i]-matriz2[i];
+	cout<<resta[i]<<"\n";
+}
+}
+
+//MULTIPLICACIÓN DE LAS MATRICES
+if(c==3)
+
+{
+	cout<<"USTED ESCOJIO LA OPCION DE MULTIPLICACION\n\n";
+	cout<<"EL RESULTADO ES:\n\n";
+	for (int i=0;i<10;i++)
+{
+	producto[i]=matriz1[i]*matriz2[i];
+	cout<<producto[i]<<"\n";
+}
+}
+
+//DIVISIÓN DE LAS MATRICES
+if(c==4)
+
+{
+	cout<<"USTED ESCOJIO LA OPCION DE DIVISION\n\n";
+	cout<<"EL RESULTADO ES:\n\n";
+	for (int i=0;i<10;i++)
+{
+	division[i]=matriz1[i]/matriz2[i];
+	cout<<division[i]<<"\n";
+}
+}
+
+
+
+return 0;
+
+}
